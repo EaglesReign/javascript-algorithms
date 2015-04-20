734a467cb14672600cc1e9307064e30ee25e8aaf
Phillip <phv9495@saintjoe.edu>
2015-04-20 09:44:28 -0400

    Add comments to make it easier to understand what's going on

--- src/sorting/heapHomework.js
+++ src/sorting/heapHomework.js
@@ -4,13 +4,16 @@
 //This program creates an array of 50 random numbers, displays it, 
 //heapsorts it, then displays it again.
 
+//store call to heapsort in variable sort
 var sort = require('./heapsort').heapSort;
 
+//create 50 random element array
 var nums = [];
 for (var i = 0; i < 50; ++i) {
   nums[i] = Math.floor(Math.random() * 51);
 }
 
+//function to display the array
 function dispArr(arr) {
   for (var i = 0; i < arr.length; ++i) {
     process.stdout.write(arr[i] + " ");
@@ -24,8 +27,8 @@ function dispArr(arr) {
   }
 }
 
-console.log("This array is unsorted: "); 
-dispArr(nums);
-sort(nums);
-console.log("This array is sorted: ");
-dispArr(nums);
+console.log("This array is unsorted: "); //label the following array as unsorted
+dispArr(nums); //display unsorted array
+sort(nums); //call to heapsort, sorts the array
+console.log("This array is sorted: "); //label array as sorted
+dispArr(nums); //display sorted array
